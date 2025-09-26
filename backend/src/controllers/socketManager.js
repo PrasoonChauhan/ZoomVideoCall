3242a04377a1d9820487a48a24b2cc895c9e01fb
Prasoon Chauhan <chauhan.prasoon132@gmail.com>
2025-09-27 03:13:11 +0530

    Ready for Production

---
+++ backend/src/controllers/socketManager.js
@@ -0,0 +1,98 @@
+import { Server } from "socket.io";
+
+let connections = {};
+let messages = {};
+let timeOnline = {};
+
+export const connectToSocket = (server) => {
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["*"],
+      credentials: true,
+    },
+  });
+
+  io.on("connection", (socket) => {
+    socket.on("join-call", (path) => {
+      if (connections[path] === undefined) {
+        connections[path] = [];
+      }
+      connections[path].push(socket.id);
+      timeOnline[socket.id] = new Date();
+
+      // Notify existing users
+      for (let a = 0; a < connections[path].length; a++) {
+        io.to(connections[path][a]).emit("user-joined", socket.id, connections[path]);
+      }
+
+      // Send old messages to new user
+      if (messages[path] !== undefined) {
+        for (let a = 0; a < messages[path].length; ++a) {
+          io.to(socket.id).emit(
+            "chat-message",
+            messages[path][a]["data"],
+            messages[path][a]["sender"],
+            messages[path][a]["socket-id-sender"]
+          );
+        }
+      }
+    });
+
+    socket.on("signal", (toId, message) => {
+      io.to(toId).emit("signal", socket.id, message);
+    });
+
+    socket.on("chat-message", (data, sender) => {
+      let machingRoom = Object.keys(connections).find((room) =>
+        connections[room].includes(socket.id)
+      );
+
+      if (machingRoom) {
+        if (messages[machingRoom] === undefined) {
+          messages[machingRoom] = [];
+        }
+        messages[machingRoom].push({
+          sender: sender,
+          data: data,
+          "socket-id-sender": socket.id,
+        });
+
+        console.log("message", machingRoom, ":", sender, data);
+
+        // Broadcast to all in room
+        connections[machingRoom].forEach((elem) => {
+          io.to(elem).emit("chat-message", data, sender, socket.id);
+        });
+      }
+    });
+
+    socket.on("disconnect", () => {
+      let key;
+
+      for (const [k, v] of Object.entries(connections)) {
+        for (let a = 0; a < v.length; ++a) {
+          if (v[a] === socket.id) {
+            key = k;
+
+            // Notify others
+            for (let b = 0; b < connections[key].length; ++b) {
+              io.to(connections[key][b]).emit("user-left", socket.id);
+            }
+
+            // Remove from connections
+            let index = connections[key].indexOf(socket.id);
+            connections[key].splice(index, 1);
+
+            if (connections[key].length === 0) {
+              delete connections[key];
+            }
+          }
+        }
+      }
+    });
+  });
+
+  return io;
+};
